4e50cc0262a1b94445e0501c78ed09b37f849174
tfletcher <tarrell.fletcher@gmail.com>
2020-06-16 16:36:44 -0400

    Created Setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='CommandTimeLiner',
+    version='',
+    packages=['lib', 'lib.db', 'lib.core', 'lib.controller'],
+    url='',
+    license='',
+    author='Tarrell Fletcher & Noah Powers',
+    author_email='',
+    description=''
+)
