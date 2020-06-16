adbb34988496ae79e6d2f6b8c0aa16b4349a508a
tfletcher <tarrell.fletcher@gmail.com>
2020-06-16 16:34:57 -0400

    Changed Interpreter Version

---
+++ command_history.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3.6
+
+# Description: Stored captured history data in database for logging and metrics
+#
+# Flow:
+# (1) Generate Program Path -> Just in case files need to be referenced in project directory
+# (2) Retrieve arguments passed in upon execution
+# (3) Send arguments to controller to start processes
+
+from lib.core.arguments import Arguments
+from lib.controller.controller import Controller
+import sys
+import os
+
+if sys.version_info < (3, 6):
+    sys.stdout.write("Requires Python 3.7\n")
+    sys.exit(1)
+
+class Program(object):
+
+    def __init__(self):
+        self.path = (os.path.dirname(os.path.realpath(__file__)))
+        self.arguments = Arguments(self.path)
+        self.controller = Controller(self.path, self.arguments)
+
+if __name__ == "__main__":
+    main = Program()
