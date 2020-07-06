77c62c7e127773a40d538bd3a5207516fd37c6ba
tfletcher <tarrell.fletcher@gmail.com>
2020-07-06 11:10:17 -0400

    Initial Push

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+import os
+
+setup(
+    name='CommandHistory',
+    version='',
+    packages=['lib', 'lib.db', 'lib.core', 'lib.controller'],
+    url='',
+    license='',
+    author='Tarrell Fletcher & Noah Powers',
+    author_email='',
+    description=''
+)
+
+os.system("python3 -m pip install -r requirements.txt")
+
+
