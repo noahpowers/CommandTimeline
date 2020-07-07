3db5900f88fb76f8e85a1b04cd6411cbab924448
tarrell13 <tarrell13@verizon.net>
2020-07-07 01:56:10 -0400

    Bunch of comments at the moment

--- setup.py
+++ setup.py
@@ -14,4 +14,12 @@ setup(
 
 os.system("python3 -m pip install -r requirements.txt")
 
+""" For Server Section """
+#os.system("/usr/lib/postgresql/10/bin/initdb -d /etc/command_history")
+#os.system("/usr/lib/postgresql/10/bin/createuser -d --host=localhost chu")
+#os.system("sudo -u postgres psql -c \"CREATE USER chu CREATEDB\"")
+#os.system("sudo -u postgres psql -c \"ALTER USER chu PASSWORD 'password';\"")
+#sudo -u postgres psql -c \"CREATE DATABASE commandhistory;\"")
+#CREATE USER john WITH ENCRYPTED PASSWORD 'Postgr@s321!';
+
 
