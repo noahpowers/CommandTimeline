110ca7558276104bb9a6690c6959a1618f2f60d3
tarrell13 <tarrell13@verizon.net>
2020-07-07 22:15:58 -0400

    Made signficant changes here, moved some methods and properties to the global area

--- lib/models/server/server.py
+++ lib/models/server/server.py
@@ -1,36 +1,41 @@
 from lib.models.globals import ServerGlobals
+from werkzeug.utils import secure_filename
 from flask import request, jsonify
+import shutil
 import threading
 import logging
 import flask
 import magic
 import time
+import sys
 import re
 import os
 
-
+allowed_extensions = ['db']
 app = flask.Flask(__name__)
+app.secret_key = 'applesauce'
+app.config['SESSION_TYPE'] = 'filesystem'
 job_queue = []
+program_path = None
 busy = False
+file = None
+
 
 """ Defines the Server Instance """
-class ServerMode(object):
+class ServerMode():
 
-    def __int__(self, path, user):
+    def __init__(self, path, user):
 
-        global app
-
-        self.path = path
+        global app, program_path
         self.user = user
-        self.allowed_extensions = ['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif']
+        program_path = path
+        app.config['UPLOAD_FOLDER'] = program_path + "/uploads/"
 
-        app.config['UPLOAD_FOLDER'] = self.path + "/uploads/"
-
-        upload_worker = threading.Thread(target=self.uploadWorker)
-        upload_worker.start()
+        """ Kick off Watch Thread For Events """
+        worker_process = threading.Thread(target=work, args=(self.user,))
+        worker_process.start()
 
     def start(self):
-
         log = logging.getLogger('werkzeug')
         log.disabled = True
         app.name = "Command History Archiver"
@@ -40,77 +45,88 @@ class ServerMode(object):
     def addHistoryFiles(self, filename):
         self.user.UnpackDatabase(filename=filename, server=True)
 
-    """ Unpack Data """
-    def unpackData(self, filename):
-        pass
-
     """ May not be necessary but wanna have one pipe line into program """
     def directRequest(self):
         pass
 
-    def allowed_file(self, filename):
-        return '.' in filename and \
-               filename.rsplit('.', 1)[1].lower() in self.allowed_extensions
 
-    """ Secures """
-    def secure_filename(self, upload, test=False):
+""" Watches the uploads directory to process and add files"""
+def work(user):
+    if not os.path.exists(program_path + "/uploads"):
+        os.makedirs(program_path + "/uploads")
 
-        if test:
+    user.arguments.server = True
+
+    while True:
+        if job_queue:
+            cache = job_queue
+            busy = True
+            for i in range(len(cache)):
+                user.UnpackData(filename=cache[i])
+
+                if i in job_queue:
+                    job_queue.pop(i)
+
+            cache = []
+            busy = False
+        else:
+            time.sleep(5)
+
+""" Retrieves the posted file """
+@app.route("/pushHistory", methods=["POST"])
+def postFile():
+    """ If FIle is Sent Over """
+    if request.method == "POST":
+        if 'file' not in request.files:
+            flask.flash("No File Data")
+            return flask.redirect(request.url)
+        if request.args.get("uploadFile"):
+
+            if request.args["uploadFile"] == "true":
+                file_upload = request.files["file"]
+            else:
+                return jsonify({400 : "Invalid Parameters"})
+
+            if file_upload:
+                if process(file_upload):
+                    return jsonify({200: "Success"})
+                else:
+                    return jsonify({400: "File Not Uploaded"})
+        else:
+            return jsonify({400: "Invalid"})
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in allowed_extensions
+
+
+def checkFileType(upload):
+
+    filename = upload.filename
+    location = os.path.join(program_path + '/uploads/temp/', filename)
+    upload.save(location)
+
+    if re.search("sqlite 3".lower(), magic.from_file(program_path + "/uploads/temp/%s" %filename).lower()):
+        shutil.move(program_path + "/uploads/temp/" + filename, program_path + "/uploads/%s" %filename)
+        return True
+    else:
+        shutil.copyfile(program_path + "/uploads/temp/%s" %filename, "/dev/null")
+        return False
+
+def process(file_upload):
+
+    file_upload.filename = secure_filename(file_upload.filename)
+    if file_upload.filename == "":
+        flask.flash("No File Selected")
+        return flask.redirect(request.url)
+
+    if file_upload and allowed_file(file_upload.filename):
+        if checkFileType(file_upload):
+
+            #file_upload.save(os.path.join(app.config['UPLOAD_FOLDER'], file_upload.filename))
+            job_queue.append(file_upload.filename)
             return True
-
-        if re.search("sqlite 3", magic.from_file(upload)):
-            return upload
         else:
             return False
-
-    """ Retrieves the posted file """
-    @app.route ("/push-history", methods=["POST"])
-    def postFile(self):
-
-        print("[*] Incoming File ")
-        """ If FIle is Sent Over """
-        if request.method == "POST":
-            if 'file' not in request.files:
-                flask.flash("No File Data")
-                return flask.redirect(request.url)
-            file = request.files["file"]
-
-            if file.filename == "":
-                flask.flash("No File Selected")
-                return flask.redirect(request.url)
-
-            if file and self.allowed_file(file.filename):
-                filename = self.secure_filename(file.filename)
-                if filename:
-                    file.save(self.path + "uploads/", filename)
-                    job_queue.append(filename)
-
-                    return flask.redirect(flask.url_for('uploaded_file',filename=filename))
-
-        return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form> '''
-
-    """ Watches the uploads directory to process and add files"""
-    def uploadWorker(self):
-
-        if not os.path.exists(self.path + "/uploads"):
-            os.makedirs(self.path + "/uploads")
-
-            while True:
-                if job_queue:
-                    self.busy = True
-                    for job in job_queue:
-                        self.user.UnpackDatabase(filename=job)
-                    self.busy = False
-                else:
-                    time.sleep(3)
-
-
-
 
